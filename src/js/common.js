a986af8efc4e4ee1e12acd98a343caa0f1089fcd
DimDimich-creator <stilys62@gmail.com>
2023-01-05 00:10:53 +0300

    added main section

---
+++ src/js/common.js
@@ -0,0 +1,10 @@
+// const { BrowserWindow } = require("electron");
+
+// // let wnd = remote.getCurrentWindow();
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     document.querySelector(".btn-close").addEventListener("click", () => {
+//         // win.close();
+//         console.log("close");
+//     });
+// });
