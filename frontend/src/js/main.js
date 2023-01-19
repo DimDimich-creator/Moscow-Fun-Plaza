d8ca61a41489b72d77ff28b269f0557d67950546
timurjumaniyazow <timurjumaniyazowtimur@gmail.com>
2023-01-19 14:32:45 +0300

    profile and card

---
+++ frontend/src/js/main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+require("@electron/remote/main").initialize();
+// const remote = require("remote");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        minWidth: 800,
+        minHeight: 600,
+        center: true,
+        // frame: false,
+        show: false,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            contextIsolation: false,
+            preload: path.join(__dirname, "js/preload.js"),
+        },
+    });
+    win.once("ready-to-show", () => {
+        win.show();
+    });
+    win.loadFile("dist/index.html");
+    // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
